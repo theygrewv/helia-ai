2ebd6387365bc40929da544b94abb8e8a57cc73a
theygrewv <ptsdhippie@gmail.com>
2026-03-15 18:48:03 -0500

    Identity: Added Service Worker shield to protect her brain from cache wipes

--- sw.js
+++ sw.js
@@ -1,10 +1,23 @@
-const cacheName = 'helia-v2';
-const assets = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'helia-sovereign-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  'https://cdn.jsdelivr.net/npm/@xenova/transformers@2.17.1',
+  'https://esm.sh/@atproto/api'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
