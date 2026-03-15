c7d4404dc3aef2bc6d7faa47fc53662a7272ca3d
theygrewv <ptsdhippie@gmail.com>
2026-03-15 17:25:03 -0500

    Evolution: Transformed into PWA for background persistence

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const cacheName = 'helia-v1';
-const assets = ['./', './index.html'];
+const cacheName = 'helia-v2';
+const assets = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
-    e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
 self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
